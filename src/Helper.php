1a5a12d1585c7e48f97975ef3ddba05dd2df4de1
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-05-21 20:26:38 +0300

    Old functionality not needed anymore

--- src/Helper.php
+++ src/Helper.php
@@ -5,34 +5,4 @@ namespace Maris;
 
 class Helper
 {
-    /**
-     * TODO: Remake to just to array to array
-     * @param $array
-     * @param $columnName
-     * @param array $primaryKey
-     * @return array
-     */
-    public static function arrayToPrimarizedArray($array, $columnName, $primaryKey = [])
-    {
-        $result = array();
-
-        foreach($array as $key => $val) {
-            if($primaryKey) {
-                $result[self::compact($primaryKey)] = $val->{$columnName};
-            } else {
-                $result[] = $val->{$columnName};
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * @param array $values
-     * @return string of concatenated values
-     */
-    public static function compact(array $values)
-    {
-        return implode("_", $values);
-    }
 }
